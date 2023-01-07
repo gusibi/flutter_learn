0a2b669dc8abfbc8525077523325260938b77dfd
gusibi <hi@gusibi.mobi>
2023-01-08 00:13:53 +0800

    learn  Flutter Tutorial For Beginners In 1 Hour

---
+++ learn_dart/dart_starter/bin/dart_start.dart
@@ -0,0 +1,5 @@
+import 'package:dart_starter/calculate.dart' as dart_application_1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_application_1.calculate()}!');
+}
---
+++ learn_dart/dart_starter/lib/01_hello_world.dart
@@ -0,0 +1,7 @@
+void hello_world() {
+  print("hello, dart world");
+}
+
+void main() {
+  hello_world();
+}
